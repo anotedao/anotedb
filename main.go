055966bef487094a6892235604749ff67d13ed33
Pragmatic <cryptopragmatic@protonmail.com>
2023-09-03 21:28:49 +0200

    db replication work

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	log.Println("done")
+	db = initDb()
+
+	var miners []*Miner
+	db.Find(&miners)
+
+	counter := 0
+
+	for range miners {
+		counter++
+	}
+
+	log.Println(counter)
 }
