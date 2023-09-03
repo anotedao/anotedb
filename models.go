ed1b610eeb19622d63f4dedba3e82627774264c6
Pragmatic <cryptopragmatic@protonmail.com>
2023-09-03 21:26:57 +0200

    db replication work

---
+++ models.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type KeyValue struct {
+	gorm.Model
+	Key      string `gorm:"size:255;uniqueIndex"`
+	ValueInt uint64 `gorm:"type:int"`
+	ValueStr string `gorm:"type:string"`
+}
+
+type Miner struct {
+	gorm.Model
+	Address                string `gorm:"size:255;uniqueIndex"`
+	LastNotification       time.Time
+	LastNotificationWeekly time.Time `gorm:"default:'2023-06-17 23:00:00.797487649+00:00'"`
+	TelegramId             int64     `gorm:"uniqueIndex"`
+	MiningHeight           int64
+	MiningTime             time.Time
+	ReferralID             uint `gorm:"index"`
+	Balance                uint64
+	MinedTelegram          uint64
+	MinedMobile            uint64
+	LastPing               time.Time
+	PingCount              int64
+	IpAddresses            []*IpAddress `gorm:"many2many:miner_ip_addresses;"`
+	UpdatedApp             bool         `gorm:"default:false"`
+	LastInvite             time.Time
+	BatteryNotification    bool `gorm:"default:false"`
+	Cycles                 uint64
+}
+
+type IpAddress struct {
+	gorm.Model
+	Address string   `gorm:"size:255;uniqueIndex"`
+	Miners  []*Miner `gorm:"many2many:miner_ip_addresses;"`
+}
